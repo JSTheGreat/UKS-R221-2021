78427e54f39482ef965603db0b348db456c161f2
JSTheGreat <jovansvorcan14@gmail.com>
2023-06-28 16:07:54 +0200

    Simple form added

--- GithubJS/GitJS/urls.py
+++ GithubJS/GitJS/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:project_id>/", views.single_project, name="single_project"),
+    path("<int:pk>/", views.ProjectView.as_view(), name="single_project"),
     path("<int:project_id>/branch/<int:branch_id>", views.single_branch, name="single_branch"),
+    path("<int:project_id>/add_branch/", views.add_branch, name="add_branch"),
 ]
