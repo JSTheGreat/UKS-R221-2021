95c06ef00fbe00e3c5fefb84d70e19b5f62721b1
JSTheGreat <jovansvorcan14@gmail.com>
2023-06-28 17:01:11 +0200

    Initial tests added

--- GithubJS/GitJS/models.py
+++ GithubJS/GitJS/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
 
+    def get_branch_number(self):
+        branches = Branch.objects.filter(project=self)
+        return len(branches)
+
 
 class Branch(models.Model):
     name = models.CharField(max_length=100)
