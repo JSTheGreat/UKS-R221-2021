13bc3153eb7b4358e4740d7ea43ab705e6f81b9d
JSTheGreat <jovansvorcan14@gmail.com>
2023-07-24 16:15:41 +0200

    Editing and deleting branches enabled for own projects

--- GithubJS/GitJS/views.py
+++ GithubJS/GitJS/views.py
@@ -1,12 +1,10 @@
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied
-from django.urls import reverse
 
-from .models import Project, Branch, GitUser, StarredProject
+from .models import Project, Branch, GitUser
 
 from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
@@ -32,33 +30,6 @@ def single_branch(request, project_id, branch_id):
     if branch.project.id != project_id:
         raise Http404("Branch doesn't match the project")
     return render(request, 'branch_view.html', {"name": branch.name, "project": project, "title": "Single branch"})
-
-
-@login_required(login_url='login/')
-@permission_required('GitJS.can_edit', raise_exception=True)
-def add_branch(request, project_id):
-    project = get_object_or_404(Project, id=project_id)
-    if request.method == 'GET':
-        return render(request, "branch_form.html", {"project": project, "title": "Add branch"})
-    else:
-        new_branch_name = request.POST['new_branch']
-        if new_branch_name.strip() == '':
-            error_message = "Branch name can't be empty"
-            return render(request, "branch_form.html", {"project": project, "error_message": error_message,
-                                                        "title": "Error!"})
-        new_id = len(Branch.objects.all())+1
-        b = Branch(id=new_id, name=new_branch_name)
-        b.project = project
-
-        try:
-            existing_branch = Branch.objects.get(name=new_branch_name, project_id=project.id)
-            error_message = "Branch name already exists"
-            return render(request, "branch_form.html", {"project": project, "error_message": error_message,
-                                                        "title": "Error!"})
-        except:
-            b.save()
-            project.update_users('Branch ' + b.name + ' added to project ' + project.title)
-            return HttpResponseRedirect(reverse("single_project", args=(project.id,)))
 
 
 def git_login(request):
